c1eb1721373e0adb33d56a04dbfbcff79bdbb71f
takara2314 <takara2314_TK@outlook.jp>
2022-06-03 17:07:52 +0900

    Debug: add

--- .github/editConfig.js
+++ .github/editConfig.js
@@ -3,7 +3,7 @@ const target = './next.config.js';
 
 // basePathを /~trashart に変更する
 let text = fs.readFileSync(target, 'utf-8');
-text = text.replace('// basePath: \'/\'', 'basePath: \'/~trashart\'');
+text = text.replace('// assetPrefix: \'/\'', 'assetPrefix: \'/~trashart\'');
 
 fs.writeFileSync(target, text, (err) => {
   if (err) throw err;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: '/'
+  // assetPrefix: '/',
+  async rewrites() {
+    return [
+      {
+        source: '/:url',
+        destination: '/~trashart/:url',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
