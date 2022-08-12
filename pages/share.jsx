d47d4b7b6ce07f08f0690c6ed2185a2618412758
chanmio121104 <19201@toba-cmt.ac.jp>
2022-08-12 20:47:36 +0900

    テキストだけ共有できるようにした

--- pages/share.jsx
+++ pages/share.jsx
@@ -1,15 +1,27 @@
 import Main from '../components/main';
+import React, { useState } from "react";
 
 const title = '共有';
 const description = '共有をしましょう！';
 
 const Share = () => {
+  const [text, setText] = useState("");
+
   return (
     <Main
       title={title}
       description={description}
     >
-      共有
+    <div className="App">
+        <input
+            className="shadow-xl border"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+        />
+        
+        <a className="mx-3 px-4 py-2 w-full text-lg font-medium text-white rounded bg-crafting hover:bg-orange-700 shadow-xl" href = {"https://twitter.com/intent/tweet?text=" + text}>Twitter</a>
+        <a className="mx-3 px-4 py-2 w-full text-lg font-medium text-white rounded bg-crafting hover:bg-orange-700 shadow-xl" href = {"https://www.facebook.com/sharer/sharer.php?u=" + text}>Facebook</a>
+        </div>
     </Main>
   );
 };
