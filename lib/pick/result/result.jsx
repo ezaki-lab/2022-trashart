8b0b28e791ba7159f1c6c98c21cffcd17400b1e7
takara2314 <takara2314_TK@outlook.jp>
2022-09-07 11:44:44 +0900

    Add: 一枚の素材画像から素材を分割して表示するように

--- lib/pick/result/result.jsx
+++ lib/pick/result/result.jsx
@@ -1,11 +1,16 @@
 import { useCallback, useEffect } from 'react';
+import { useAtom } from 'jotai';
 import { MdMemory } from 'react-icons/md';
 import { Headline1 } from '../../../components/headline';
 import Linking from '../../../components/linking';
 import useSession from '../../../hooks/useSession';
+import { materialB64Atom, materialsAtom } from '../../../models/stores';
 
 const Result = () => {
   const { setSection, setMode } = useSession();
+
+  const [materialB64, setMaterialB64] = useAtom(materialB64Atom);
+  const [materials, setMaterials] = useAtom(materialsAtom);
 
   const retake = useCallback(() => {
     setSection('camera');
@@ -13,6 +18,25 @@ const Result = () => {
 
   useEffect(() => {
     setMode('result');
+
+    if (materialB64 === '') {
+      return;
+    }
+
+    fetch(process.env.NEXT_PUBLIC_API_URL + '/pick/store', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'data': materialB64
+      })
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setMaterials(json['materials']);
+      });
   }, []);
 
   return (
@@ -27,7 +51,24 @@ const Result = () => {
 
       これだけの素材が集まりました。
 
-      <div className="mt-5 w-32 h-32 bg-picking-700" />
+      {materials.length === 0
+        ? (
+          <div className="mt-5 w-32 h-32 bg-picking-700" />
+        )
+        : (
+          <div className="w-full flex flex-wrap">
+            {materials.map((material) =>
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={material['image_url']}
+                alt="素材画像"
+                className="p-3"
+                key={material['id']}
+              />
+            )}
+          </div>
+        )
+      }
 
       <div className="w-full h-40 fixed left-0 bottom-0">
         <div className="mt-10">
