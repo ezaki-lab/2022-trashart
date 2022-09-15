07e2851bbd135d6622aeee6f65dee4eae46e3f94
takara2314 <takara2314_TK@outlook.jp>
2022-09-15 23:38:23 +0900

    Add: `album`

--- pages/album.jsx
+++ pages/album.jsx
@@ -1,4 +1,6 @@
 import Session from '../lib/session';
+import MyAlbum from '../lib/album/myAlbum';
+import url from '../utils/url';
 
 const title = 'アルバム';
 const description = 'アルバムを見ましょう！';
@@ -8,8 +10,19 @@ const Album = () => {
     <Session
       title={title}
       description={description}
+      className="h-[calc(100%-5rem)] flex flex-col items-center overflow-x-hidden overflow-y-auto"
+      style={{
+        background: 'radial-gradient(rgb(234, 241, 246) 0.2rem, transparent 0.2rem)',
+        backgroundSize: '3rem 3rem'
+      }}
     >
-      アルバム
+      <img
+        src={url('/logo.svg')}
+        alt="ロゴ"
+        className="my-10 w-1/2"
+      />
+
+      <MyAlbum />
     </Session>
   );
 };
