cc9b876578ae2c4ea3b8cbc113e1dd57e2e1ebbd
takara2314 <takara2314_TK@outlook.jp>
2022-10-05 23:31:46 +0900

    Improve: 共有機能を充実

--- lib/craft/share/shareButton.jsx
+++ lib/craft/share/shareButton.jsx
@@ -4,17 +4,10 @@ import { useAtom } from 'jotai';
 import { FiShare2 } from 'react-icons/fi';
 import { userIdAtom, titleAtom, hashtagsAtom, sharePhotoIdAtom } from '../../../models/stores';
 import url from '../../../utils/url';
-import { pickRandom } from '../../../utils/random';
 import useSession from '../../../hooks/useSession';
 import Modal from './modal';
 import api from '../../../models/apiClient';
-
-const messages = [
-  '実は日本の漂着ごみの6割が漁具です。',
-  '2050年には、海洋ごみが魚の数を越えると言われています。',
-  '海のプラスチックを正しく分別すると、処理費用を抑えることができます。',
-  'プラスチックの中でもポリプロピレンは分解されにくいです。'
-];
+import share from '../../share';
 
 const ShareToSns = () => {
   const { setSection, resetSession } = useSession();
@@ -41,17 +34,7 @@ const ShareToSns = () => {
   }, [userId]);
 
   const handleShare = useCallback(() => {
-    if (window.location.protocol !== 'https:') {
-      return;
-    }
-
-    const snsHashtags = hashtags.map((hashtag) => `#${hashtag}`).join(' ');
-
-    navigator.share({
-      title: 'MARINE TRASHART',
-      text: `「${title}」を作りました！ ${snsHashtags} ${pickRandom(messages)}`,
-      url: `${process.env.NEXT_PUBLIC_URL}?id=${sharePhotoId}`,
-    });
+    share(title, sharePhotoId, hashtags);
   }, [title, sharePhotoId, hashtags]);
 
   const handleShared = useCallback(() => {
